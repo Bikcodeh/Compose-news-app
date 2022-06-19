6cafaa447fe0c7ee419f17bc4759950ee737caf9
unknown <bikcodeh@gmail.com>
2022-06-19 11:17:55 -0500

    initial setup

---
+++ app/src/main/java/com/bikcodeh/newsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.bikcodeh.newsapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.bikcodeh.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+    val context = LocalContext.current
+    Toast.makeText(context, BuildConfig.APP_CENTER_KEY, Toast.LENGTH_SHORT).show()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NewsAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/bikcodeh/newsapp/NewsAppApplication.kt
@@ -0,0 +1,25 @@
+package com.bikcodeh.newsapp
+
+import android.app.Application
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NewsAppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupAppCenter()
+    }
+
+    private fun setupAppCenter() {
+        AppCenter.start(
+            this,
+            BuildConfig.APP_CENTER_KEY,
+            Analytics::class.java,
+            Crashes::class.java
+        )
+    }
+}
