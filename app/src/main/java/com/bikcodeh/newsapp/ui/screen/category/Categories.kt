debee4437030c0c1827bd57425e09460ef43abe2
Bikcode <bikcodeh@gmail.com>
2022-06-20 10:47:35 -0500

    create article content component

--- app/src/main/java/com/bikcodeh/newsapp/ui/screen/category/Categories.kt
+++ app/src/main/java/com/bikcodeh/newsapp/ui/screen/category/Categories.kt
@@ -1,9 +1,12 @@
 package com.bikcodeh.newsapp.ui.screen.category
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,10 +14,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bikcodeh.newsapp.R
+import com.bikcodeh.newsapp.data.model.TopNewsArticle
+import com.bikcodeh.newsapp.domain.model.MockData
+import com.bikcodeh.newsapp.domain.model.MockData.getTimeAgo
 import com.bikcodeh.newsapp.domain.model.getAllArticleCategory
 import com.bikcodeh.newsapp.ui.screen.home.NewsManager
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun CategoriesScreen(onFetchCategory: (String) -> Unit, newsManager: NewsManager) {
@@ -57,4 +68,60 @@ fun CategoryTab(
             modifier = Modifier.padding(8.dp)
         )
     }
+}
+
+@Composable
+fun ArticleContent(articles: List<TopNewsArticle>, modifier: Modifier = Modifier) {
+    LazyColumn() {
+        items(articles) { article ->
+            Card(
+                modifier.padding(8.dp),
+                border = BorderStroke(2.dp, color = colorResource(id = R.color.purple_500))
+            ) {
+                Row(
+                    modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    CoilImage(
+                        imageModel = article.urlToImage,
+                        modifier = Modifier.size(100.dp),
+                        placeHolder = painterResource(id = R.drawable.breaking_news),
+                        error = painterResource(id = R.drawable.breaking_news)
+                    )
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        Text(
+                            text = article.title ?: "Not Available", fontWeight = FontWeight.Bold,
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(text = article.author ?: "Not Available")
+                            article.publishedAt?.let {
+                                Text(text = MockData.stringToDate(article.publishedAt).getTimeAgo())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ArticleContentPreview() {
+    ArticleContent(
+        articles = listOf(
+            TopNewsArticle(
+                author = "Namita Singh",
+                title = "Cleo Smith news — live: Kidnap suspect 'in hospital again' as 'hard police grind' credited for breakthrough - The Independent",
+                description = "The suspected kidnapper of four-year-old Cleo Smith has been treated in hospital for a second time amid reports he was “attacked” while in custody.",
+                publishedAt = "2021-11-04T04:42:40Z"
+            )
+        )
+    )
 }
