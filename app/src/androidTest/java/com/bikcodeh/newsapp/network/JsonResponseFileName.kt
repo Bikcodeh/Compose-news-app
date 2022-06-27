b746fa6c2c7a2e5117de1e509f6dcbc48656917b
Bikcode <bikcodeh@gmail.com>
2022-06-27 12:59:15 -0500

    start implementation ui testing for sources screen

--- app/src/androidTest/java/com/bikcodeh/newsapp/network/JsonResponseFileName.kt
+++ app/src/androidTest/java/com/bikcodeh/newsapp/network/JsonResponseFileName.kt
@@ -2,3 +2,5 @@ package com.bikcodeh.newsapp.network
 
 const val FILE_SUCCESS_NEWS_RESPONSE = "success_news_response.json"
 const val FILE_SUCCESS_EMPTY_RESPONSE = "success_empty_response.json"
+const val FILE_SUCCESS_NEWS_BY_SOURCE_RESPONSE = "success_news_by_source_response.json"
+const val FILE_SUCCESS_NEWS_BY_SOURCE_TALK_SPORT_RESPONSE = "success_news_by_source_talksport_response.json"
--- app/src/androidTest/java/com/bikcodeh/newsapp/network/RequestPath.kt
+++ app/src/androidTest/java/com/bikcodeh/newsapp/network/RequestPath.kt
@@ -1,3 +1,4 @@
 package com.bikcodeh.newsapp.network
 
 const val SUCCESS_NEWS_RESPONSE_PATH = "/top-headlines?country=us"
+const val SUCCESS_NEWS_BY_SOURCE_RESPONSE_PATH = "/everything?source=abc-news"
